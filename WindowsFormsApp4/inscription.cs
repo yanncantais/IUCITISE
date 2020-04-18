b14adec4c346deec865071d3bdd42eac7eb571ee
yanncantais <lurtzdelagriffeblanche@gmail.com>
2020-04-18 10:08:59 +0200

    Corrigé un bug dans inscription

--- WindowsFormsApp4/inscription.cs
+++ WindowsFormsApp4/inscription.cs
@@ -14,13 +14,14 @@ namespace WindowsFormsApp4
     
     public partial class inscription : Form
     {
+        //On ouvre une connexion avec la base de donnée
         NpgsqlConnection con = new NpgsqlConnection("Server=87.91.82.229;Port=5432;" + "User Id=yannMax;Password=admin;Database=database1");
         NpgsqlCommand cmd;
         public inscription()
         {
             InitializeComponent();
         }
-
+        #region Déclaration de fonctions sans définition
         private void Form2_Load(object sender, EventArgs e)
         {
 
@@ -59,25 +60,28 @@ namespace WindowsFormsApp4
         {
 
         }
-
+        #endregion
+        //Bouton pour fermer la fenetre
         private void btn_quitter_Click(object sender, EventArgs e)
         {
-            this.Close();
+            this.Close();//On ferme cette form
             foreach (Form form in Application.OpenForms)
             {
-                if (form.Name == "accueil")
+                if (form.Name == "accueil")//On ouvre à la place le form accueil
                 {
                     form.Show();
                 }
             }
         }
-
+        //Bouton pour valider l'inscription
         private void btn_valider_Click(object sender, EventArgs e)
         {
             btn_valider.Enabled = false;
+            con.Close();
             con.Open();
-            if (txtbx_mdp.Text == txtbx_cmdp.Text)
+            if (txtbx_mdp.Text == txtbx_cmdp.Text)//concordace des deux champs ou on rentre le mdp
             {
+                //On insère ensuite les valeurs renseignées dans la base de donnée
                 cmd = new NpgsqlCommand("insert into citise2 (Nom, Prenom, Password, Groupe, Idetu, Question)" + " values (@Nom, @Prenom, @Password, @Groupe, @Idetu, @Question)", con);
                 cmd.Parameters.AddWithValue("@Nom", txtbx_nom.Text);
                 cmd.Parameters.AddWithValue("@Prenom", txtbx_prenom.Text);
@@ -85,9 +89,9 @@ namespace WindowsFormsApp4
                 cmd.Parameters.AddWithValue("@Groupe", cb_groupe.SelectedItem);
                 cmd.Parameters.AddWithValue("@Idetu", txtbx_numetu.Text);
                 cmd.Parameters.AddWithValue("@Question", txtbx_qstn.Text);
-                cmd.ExecuteNonQuery();
+                cmd.ExecuteNonQuery();//On execute la requete
                 MessageBox.Show("Vous avez fini votre inscription avec succès");
-                btn_valider_Click(sender, e);
+                btn_quitter_Click(sender, e);//On ferme alors la fenetre d'inscription comme si on clickait sur le bouton pour la fermer-->on revient à l'accueil
             }
             else
             {
